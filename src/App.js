db43fac47558fd77dd3ce5e04e56e770c9228668
William M. Tsikata <tsikatawill@gmail.com>
2022-08-09 14:20:58 +0000

    add some filler text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,90 @@
 import Header from './components/Header'
 
 export default function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <div className="spacer">
+        <div className="container py-10">
+          <h2 className="text-xl font-bold">Some title</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Exercitationem architecto in, amet temporibus quam ipsam soluta enim
+            nemo delectus dolor, accusantium error hic a nam doloribus porro
+            optio fuga minus beatae iusto. Ullam suscipit libero nesciunt,
+            dignissimos praesentium modi fugiat veritatis consequatur ad commodi
+            in similique, error aut ipsa quos mollitia, enim expedita numquam
+            facilis alias porro. Necessitatibus tempore assumenda, excepturi,
+            eveniet distinctio nobis cum similique, eum officiis nisi aspernatur
+            doloremque in molestiae eaque laborum minus repellat? Repellendus,
+            saepe debitis? Doloremque non enim iusto nesciunt molestiae
+            architecto distinctio delectus modi libero. Possimus excepturi velit
+            omnis repellendus fuga delectus iusto iure odit quia, vel, nam
+            impedit obcaecati ab earum. Animi placeat commodi minus id
+            aspernatur corrupti repellat, provident sunt esse tempore dolorum
+            quibusdam quas unde voluptate perferendis? At sint obcaecati quis
+            porro corporis? Accusantium praesentium laudantium consectetur
+            facilis suscipit reprehenderit repellendus reiciendis sequi nobis
+            provident, voluptates doloribus nostrum soluta quo a possimus! Fugit
+            atque quasi nostrum sint nemo quia obcaecati recusandae quaerat
+            officia dicta, aliquam repudiandae culpa eius dolorem provident
+            corporis dignissimos modi error non esse accusamus quas assumenda
+            iste rerum. Dignissimos aliquid suscipit eos asperiores est culpa
+            officia dicta, aspernatur repellendus ipsam amet deserunt possimus
+            magni rerum impedit aliquam placeat enim corrupti perspiciatis vel
+            vero deleniti? Veniam repellendus ad aliquam aliquid, doloremque
+            ipsam? Necessitatibus, eligendi eum cupiditate eos quam sed illum
+            harum quaerat ad error! Enim mollitia voluptates cum voluptas eius
+            molestias recusandae odio, quasi, necessitatibus nesciunt assumenda,
+            nobis consequatur! Sit libero nesciunt ab perferendis, consectetur
+            labore molestias dolorum ea minima modi, delectus perspiciatis, amet
+            maiores repellendus mollitia similique recusandae asperiores
+            exercitationem beatae. Dolores, dicta qui? Tenetur doloribus ducimus
+            quisquam officia odit, corporis labore nemo atque aspernatur,
+            maiores molestiae ipsum debitis quidem nobis, amet eos nisi
+            veritatis exercitationem alias. Nihil exercitationem debitis
+            ratione, voluptates sit consectetur possimus eaque modi sequi?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Exercitationem architecto in, amet temporibus quam ipsam soluta enim
+            nemo delectus dolor, accusantium error hic a nam doloribus porro
+            optio fuga minus beatae iusto. Ullam suscipit libero nesciunt,
+            dignissimos praesentium modi fugiat veritatis consequatur ad commodi
+            in similique, error aut ipsa quos mollitia, enim expedita numquam
+            facilis alias porro. Necessitatibus tempore assumenda, excepturi,
+            eveniet distinctio nobis cum similique, eum officiis nisi aspernatur
+            doloremque in molestiae eaque laborum minus repellat? Repellendus,
+            saepe debitis? Doloremque non enim iusto nesciunt molestiae
+            architecto distinctio delectus modi libero. Possimus excepturi velit
+            omnis repellendus fuga delectus iusto iure odit quia, vel, nam
+            impedit obcaecati ab earum. Animi placeat commodi minus id
+            aspernatur corrupti repellat, provident sunt esse tempore dolorum
+            quibusdam quas unde voluptate perferendis? At sint obcaecati quis
+            porro corporis? Accusantium praesentium laudantium consectetur
+            facilis suscipit reprehenderit repellendus reiciendis sequi nobis
+            provident, voluptates doloribus nostrum soluta quo a possimus! Fugit
+            atque quasi nostrum sint nemo quia obcaecati recusandae quaerat
+            officia dicta, aliquam repudiandae culpa eius dolorem provident
+            corporis dignissimos modi error non esse accusamus quas assumenda
+            iste rerum. Dignissimos aliquid suscipit eos asperiores est culpa
+            officia dicta, aspernatur repellendus ipsam amet deserunt possimus
+            magni rerum impedit aliquam placeat enim corrupti perspiciatis vel
+            vero deleniti? Veniam repellendus ad aliquam aliquid, doloremque
+            ipsam? Necessitatibus, eligendi eum cupiditate eos quam sed illum
+            harum quaerat ad error! Enim mollitia voluptates cum voluptas eius
+            molestias recusandae odio, quasi, necessitatibus nesciunt assumenda,
+            nobis consequatur! Sit libero nesciunt ab perferendis, consectetur
+            labore molestias dolorum ea minima modi, delectus perspiciatis, amet
+            maiores repellendus mollitia similique recusandae asperiores
+            exercitationem beatae. Dolores, dicta qui? Tenetur doloribus ducimus
+            quisquam officia odit, corporis labore nemo atque aspernatur,
+            maiores molestiae ipsum debitis quidem nobis, amet eos nisi
+            veritatis exercitationem alias. Nihil exercitationem debitis
+            ratione, voluptates sit consectetur possimus eaque modi sequi?
+          </p>
+        </div>
+      </div>
+    </>
+  )
 }
