8225df255b49ae264f64ba68ad730579c98f62d8
William M. Tsikata <tsikatawill@gmail.com>
2022-08-09 12:38:12 +0000

    add separate animation(s) js file

---
+++ src/animationVariants/index.js
@@ -0,0 +1,63 @@
+export const fadeIn = (direction = 'up', duration = 0.4) => {
+  return {
+    initial: {
+      y: direction === 'up' ? 40 : -40,
+      opacity: 0,
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration,
+        ease: 'easeInOut',
+        type: 'spring',
+      },
+    },
+  }
+}
+
+export const staggerContainer = {
+  initial: {},
+  animate: {
+    transition: { staggerChildren: 0.15, delayChildren: 0.4 },
+  },
+}
+
+export const shake = {
+  initial: {},
+  animate: (custom) => ({
+    x: [20, 0, 20],
+    rotate: [5, 0, 5],
+    transition: {
+      type: 'linear',
+      repeat: Infinity,
+      duration: custom,
+    },
+  }),
+}
+
+export const dropSpring = {
+  initial: {
+    y: -100,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: { duration: 2, type: 'spring' },
+  },
+}
+
+export const verticalOccilation = {
+  initial: {},
+  animate: {
+    y: [-20, 0, -20],
+    // rotate: [2, 0, 2],
+    transition: {
+      // delay: 0.8,
+      duration: 1,
+      type: 'spring',
+      repeat: Infinity,
+    },
+  },
+}
