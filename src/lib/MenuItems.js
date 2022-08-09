975548aedd1aacfba70888f7017e4c229744134a
William M. Tsikata <tsikatawill@gmail.com>
2022-08-09 21:22:24 +0000

    create seperate file for menu item exports

---
+++ src/lib/MenuItems.js
@@ -0,0 +1,90 @@
+import Burger1 from '../images/burger1.png'
+import Burger2 from '../images/burger2.png'
+import Burger3 from '../images/burger3.png'
+import Burger4 from '../images/burger4.png'
+import Burger5 from '../images/burger5.png'
+import Burger6 from '../images/burger6.png'
+
+import Sandwich1 from '../images/sandwich1.png'
+import Sandwich2 from '../images/sandwich2.png'
+import Sandwich3 from '../images/sandwich3.png'
+import Sandwich4 from '../images/sandwich4.png'
+import Sandwich5 from '../images/sandwich5.png'
+import Sandwich6 from '../images/sandwich6.png'
+
+export const Sandwiches = [
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Sandwich1,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Sandwich2,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Sandwich3,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Sandwich4,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Sandwich5,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Sandwich6,
+    price: 21,
+  },
+]
+export const Burgers = [
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Burger1,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Burger2,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Burger3,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Burger4,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Burger5,
+    price: 21,
+  },
+  {
+    name: 'Some mane',
+    description: 'sosadd',
+    image: Burger6,
+    price: 21,
+  },
+]
