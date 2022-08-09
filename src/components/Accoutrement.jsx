e04afcb9c45ff8d5a64246f44dab830292b4b950
William M. Tsikata <tsikatawill@gmail.com>
2022-08-09 14:21:47 +0000

    animate while in view

--- src/components/Accoutrement.jsx
+++ src/components/Accoutrement.jsx
@@ -3,11 +3,15 @@ import { dropSpring, shake } from '../animationVariants'
 
 export default function Accoutrement({ img, className, speed }) {
   return (
-    <motion.div variants={dropSpring} className={className}>
+    <motion.div
+      variants={dropSpring}
+      className={`${className} absolute scale-50 sm:scale-90`}
+    >
       <motion.img
         variants={shake}
         initial="initial"
-        animate="animate"
+        whileInView="animate"
+        // animate="animate"
         custom={speed}
         src={img}
         alt="acc.jpg"
