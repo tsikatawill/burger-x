1c9b19ee09bb9b89ae03ef36e22ab233b7469119
William M. Tsikata <tsikatawill@gmail.com>
2022-08-10 10:25:36 +0000

    add drinks to menu

--- src/lib/MenuItems.js
+++ src/lib/MenuItems.js
@@ -5,6 +5,13 @@ import Burger4 from '../images/burger4.png'
 import Burger5 from '../images/burger5.png'
 import Burger6 from '../images/burger6.png'
 
+import Drink1 from '../images/drink1.webp'
+import Drink2 from '../images/drink2.webp'
+import Drink3 from '../images/drink3.webp'
+import Drink4 from '../images/drink4.webp'
+import Drink5 from '../images/drink5.webp'
+import Drink6 from '../images/drink6.webp'
+
 import Sandwich1 from '../images/sandwich1.png'
 import Sandwich2 from '../images/sandwich2.png'
 import Sandwich3 from '../images/sandwich3.png'
@@ -14,77 +21,135 @@ import Sandwich6 from '../images/sandwich6.png'
 
 export const Sandwiches = [
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'Veggie Sandwich',
+    description:
+      'All veggie sandwich liquid necessitatibus beatae, aliquam eaque odit blanditiis odio saepe amet.',
     image: Sandwich1,
-    price: 21,
+    price: 19.99,
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'Straight Sandwich',
+    description:
+      'Modi incidunt nobis nulla natus repellat illum quam! Facilis, voluptatem accusantium. Assumenda tempora commodi quos.',
     image: Sandwich2,
-    price: 21,
+    price: 19.99,
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'SW Jnr',
+    description:
+      'Modi obcaecati, necessitatibus nobis porro laborum suscipit officiis iste excepturi labore quae maiores deleniti pariatur.sosadd',
     image: Sandwich3,
-    price: 21,
+    price: 15.99,
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'A-Boy',
+    description:
+      'Deserunt officiis excepturi necessitatibus voluptates consectetur, hic doloribus.',
     image: Sandwich4,
-    price: 21,
+    price: 24.99,
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'Double Decker',
+    description:
+      'Consectetur quod soluta corporis quasi? Voluptate, ullam recusandae magnam rerum autem.',
     image: Sandwich5,
-    price: 21,
+    price: 24.99,
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'Multiverse',
+    description:
+      'Laudantium veritatis obcaecati, id neque, deleniti iste corrupti et dolorem. Dolorem, impedit suscipit.',
     image: Sandwich6,
-    price: 21,
+    price: 24.99,
   },
 ]
+
 export const Burgers = [
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'Burger X',
     image: Burger1,
-    price: 21,
+    price: 19.99,
+    description:
+      'All veggie sandwich liquid necessitatibus beatae, aliquam eaque odit blanditiis odio saepe amet. Dicta, sunt corporis!',
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'This',
     image: Burger2,
-    price: 21,
+    price: 19.99,
+    description:
+      'Modi incidunt nobis nulla natus repellat illum quam! Facilis, voluptatem error vel.',
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'Redux Persist',
     image: Burger3,
-    price: 21,
+    price: 19.99,
+    description:
+      'Modi obcaecati, necessitatibus nobis porro laborum suscipit officiis iste excepturi.',
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'React Burger',
     image: Burger4,
-    price: 21,
+    price: 24.99,
+    description:
+      'Deserunt officiis excepturi necessitatibus voluptates consectetur, hic doloribus.',
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'Tower of Ldn',
     image: Burger5,
-    price: 21,
+    price: 24.99,
+    description:
+      'Consectetur quod soluta corporis quasi? Voluptate, ullam recusandae.',
   },
   {
-    name: 'Some mane',
-    description: 'sosadd',
+    name: 'Wittie Burg',
     image: Burger6,
-    price: 21,
+    price: 24.99,
+    description:
+      'Modi obcaecati, necessitatibus nobis porro laborum suscipit officiis iste ',
+  },
+]
+
+export const Drinks = [
+  {
+    name: 'Coca Cola',
+    image: Drink1,
+    price: 12.99,
+    description:
+      'Liquid necessitatibus beatae, aliquam eaque odit blanditiis odio saepe amet.',
+  },
+  {
+    name: 'Diet Coke',
+    image: Drink2,
+    price: 9.99,
+    description:
+      'Modi incidunt nobis nulla natus repellat illum quam! Facilis, voluptatem',
+  },
+  {
+    name: 'Iced Tea',
+    image: Drink3,
+    price: 9.99,
+    description:
+      'Modi obcaecati, necessitatibus nobis porro laborum suscipit officiis iste.',
+  },
+  {
+    name: 'Sprite',
+    image: Drink4,
+    price: 12.99,
+    description:
+      'Deserunt officiis excepturi necessitatibus voluptates consectetur, hic doloribus.',
+  },
+  {
+    name: 'Dr. Pepper',
+    image: Drink5,
+    price: 9.99,
+    description:
+      'Consectetur quod soluta corporis quasi? Voluptate, ullam recusandae.',
+  },
+  {
+    name: "Barg's",
+    image: Drink6,
+    price: 9.99,
+    description:
+      'Modi obcaecati, necessitatibus nobis porro laborum suscipit officiis iste.',
   },
 ]
