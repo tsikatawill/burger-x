649e54454459b0016bc69f0d46f54958f8ea112f
William M. Tsikata <tsikatawill@gmail.com>
2022-08-10 10:20:56 +0000

    handle animation exit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import { AnimatePresence } from 'framer-motion'
 import Header from './components/Header'
 import Menu from './components/Menu'
 
 export default function App() {
   return (
-    <>
+    <AnimatePresence>
       <Header />
       <Menu />
-    </>
+    </AnimatePresence>
   )
 }
