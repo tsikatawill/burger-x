aba26dd03f88c902a5336632ec4471c36a4e9c34
William M. Tsikata <tsikatawill@gmail.com>
2022-08-10 14:18:10 +0000

    add correct loactions
    tema
    circle
    kasoa

--- src/components/Locations.jsx
+++ src/components/Locations.jsx
@@ -7,15 +7,15 @@ export default function Locations() {
   const maps = [
     {
       location: 'Tema',
-      src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.597323833828!2d-0.2762953515404934!3d5.626308295901302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf98c25ce18e1d%3A0xa488794859304f7!2sPentecost%20University!5e0!3m2!1sen!2sgh!4v1658745265782!5m2!1sen!2sgh',
+      src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d127046.1539157831!2d-0.0586825151956617!3d5.685324651951139!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x102078a5400d3e29%3A0xccdf4b5d4026488b!2sTema!5e0!3m2!1sen!2sgh!4v1660140800715!5m2!1sen!2sgh',
     },
     {
       location: 'Kasoa',
-      src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.597323833828!2d-0.2762953515404934!3d5.626308295901302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf98c25ce18e1d%3A0xa488794859304f7!2sPentecost%20University!5e0!3m2!1sen!2sgh!4v1658745265782!5m2!1sen!2sgh',
+      src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31770.16987853918!2d-0.4368015000000098!3d5.526717800000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdfbaedda8d54f7%3A0xd48e16c6c340829a!2sKasoa!5e0!3m2!1sen!2sgh!4v1660140974610!5m2!1sen!2sgh',
     },
     {
       location: 'Circle',
-      src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.597323833828!2d-0.2762953515404934!3d5.626308295901302!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf98c25ce18e1d%3A0xa488794859304f7!2sPentecost%20University!5e0!3m2!1sen!2sgh!4v1658745265782!5m2!1sen!2sgh',
+      src: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63535.71012243477!2d-0.2814770687500262!3d5.569695699999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf99e34a453019%3A0xef3607e9a96eff13!2sKwame%20Nkrumah%20Circle!5e0!3m2!1sen!2sgh!4v1660140921554!5m2!1sen!2sgh',
     },
   ]
 
