cce3233670d015f94b8d5b87d64a952e61e0738c
William M. Tsikata <tsikatawill@gmail.com>
2022-08-25 19:06:48 +0000

    separate logo from text at footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ import {
   FaInstagram,
   FaTiktok,
   FaTwitter,
-  FaYoutube,
+  FaYoutube
 } from 'react-icons/fa'
 
 export default function Footer() {
   return (
     <footer className="bg-neutral-900">
       <div className="container py-10 flex flex-col sm:flex-row gap-7 sm:gap-0 justify-between items-center sm:items-start">
-        <div className="flex items-center flex-col sm:flex-row text-white">
+        <div className="flex items-center flex-col sm:flex-row text-white gap-5">
           <img src={Logo} alt="logo" className="w-20 sm:w-32" />
           <div className="text-center sm:text-left">
             <span className="font-bold text-3xl">Burger-X</span>
